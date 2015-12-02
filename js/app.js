edf1991a7b31647739a7f22974c678841dada2d7
Joe Devlin <joe@vividventures.biz>
2015-12-01 16:49:15 -0800

    Add angular tags to titlebar + form

--- js/app.js
+++ js/app.js
@@ -1,8 +1,27 @@
-// xml-resume 0.01
-// filename: app.js
+/* Project:  xml-resume 
+   Filename: app.js
+   Version:  V0.0.1-pre-alpha
+   By:       Joe Devlin
+   Company:  Vivid Ventures LLC
+*/
 
 /* Dependencies:
      Angular
      jQuery
      bootstrap
 */
+
+angular.module('resumeApp', [])
+   .controller('titlebar', function ($scope) {
+       if (angular.version.full != "") {
+           //document.getElementById("AngularJS-small").style.visibility="visible" ;
+           $scope.showlogo=true;
+           console.log("In resumeApp angular.version.full: " + angular.version.full);
+       }   	
+   	})
+   .controller('candidatePersonCtrl', function ($scope) {
+      // Manage input from the candidatePerson form
+      $scope.personname = {
+          preferredsalutationcode : "",
+      };
+   });
