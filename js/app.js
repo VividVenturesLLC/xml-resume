c621d12495aa179104e04db84435c18afc7bab92
Joe Devlin <joe@vividventures.biz>
2016-01-04 14:40:47 -0800

    Change to ui-bootstrap tab navigation

--- js/app.js
+++ js/app.js
@@ -1,28 +1,39 @@
 /* Project:  xml-resume
    Filename: app.js
-   Version:  V0.0.1-pre-alpha
+   Version:  V0.0.2-pre-alpha
    By:       Joe Devlin
    Company:  Vivid Ventures LLC
 */
 
 /* Dependencies:
      Angular
-     jQuery
-     bootstrap
+     ui-bootstrap
 */
 
-angular.module('resumeApp', [])
+angular.module('resumeApp', ['ngAnimate', 'ui.bootstrap'])
    .controller('titlebarCtrl', function ($scope) {
        if (angular.version.full != "") {
            $scope.showlogo=true;
            console.log("In resumeApp angular.version.full: " + angular.version.full);
        }
    	})
-   .controller('candidatePersonCtrl', function ($scope) {
-      // Link model to the candidatePerson form
-      // access the model file in ../model/candidatePerson.json
-      //$scope.candidatePerson = {};
-      $scope.personname = {
-          preferredsalutationcode : "",
-      };
-   });
+  .controller('tabViewCtrl', function ($scope, $window) {
+    $scope.tabs = [
+      { title:'Form', content:'formView' },
+      { title:'HTML', content:'htmlTemplateView'},
+      { title:'Text', content:'textTemplateView'}
+      { title:'Report', content:'reportView'}
+      { title:'JSON', content:'jsonView'}
+      { title:'XML', content:'xmlView'}
+      { title:'DOCX', content:'docxView', disabled: true}
+      { title:'PDF', content:'pdfView', disabled: true}
+      { title:'Search', content:'searchView', disabled: true}
+
+    ];
+
+  $scope.alertMe = function() {
+    setTimeout(function() {
+      $window.alert('You\'ve selected the alert tab!');
+    });
+  };
+});
