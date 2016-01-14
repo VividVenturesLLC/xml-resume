ca7d63f67ea4cba169ab7d5eadc60f84697fa087
Joe Devlin <joe@vividventures.biz>
2016-01-14 09:56:49 -0800

    Linked form to html view
    Add top level $scope in mainController to allow $scope in
    other controllers to have access to the model, and add a
    debug pane in form view to see JSON model as it updates.

--- js/app.js
+++ js/app.js
@@ -18,26 +18,54 @@ var resumeApp = angular.module('resumeApp', ['ngRoute'])
            console.log("In resumeApp angular.version.full: " + angular.version.full);
        }
    	})
-   .controller('candidatePersonCtrl', function ($scope) {
+   .controller('mainController', function($scope, $route, $routeParams, $location) {
+      	// https://docs.angularjs.org/api/ngRoute/service/$route#!
+       $scope.$route = $route;
+       $scope.$location = $location;
+       $scope.$routeParams = $routeParams;
+       /*if CandidatePerson object does not exist on this scope then create it
+       */
+      console.log("$scope.CandidatePerson:");//debug only
+      console.dir($scope.CandidatePerson);//debug only
+
+
+       if ($scope.CandidatePerson === undefined) {
+       	$scope.CandidatePerson={};
+       	console.log('Creating object $scope.CandidatePerson in mainController');//debug only
+       	}
+
+      console.log("$scope in mainController:");//debug only
+      console.dir($scope);//debug only
+
+   })
+    	
+   .controller('cpFormCtrl', function ($scope, $routeParams) {
       // Link model to the candidatePerson form
-      // access the model file in ../model/candidatePerson.json
-      //$scope.candidatePerson = {};
-      /*
-      $scope.personname = {
-          preferredsalutationcode : "",
-      };
-      */
+      $scope.name = "cpFormCtrl";
+      $scope.params = $routeParams;       
+      
+      console.log("$scope in cpFormCtrl:");//debug only
+      console.dir($scope);//debug only
+   })
+
+   .controller('htmlController', function ($scope, $routeParams) {
+      // Link model to the candidatePerson form
+      $scope.name = "htmlController";
+      $scope.params = $routeParams;
+
+      console.log("$scope in htmlController:");//debug only
+      console.dir($scope);//debug only
    });
 
 
 
-   
+
 resumeApp
   .config(['$routeProvider', function($routeProvider) {
     $routeProvider.
       when('/formView', {
         templateUrl: 'views/form_view.html',
-        controller: 'formController'
+        controller: 'cpFormCtrl'
       }).
       when('/htmlTemplateView', {
         templateUrl: 'views/templateChronological.html',
@@ -48,6 +76,6 @@ resumeApp
         controller: 'modelController'
       }).
       otherwise({
-        redirectTo: 'index.html'
+        redirectTo: 'views/form_view.html'
       });
   }]);
